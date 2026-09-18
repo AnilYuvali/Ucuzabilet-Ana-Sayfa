bc39be6bed1162ec03ac829ccc89ff9ea33f007b
AnilYuvali <anilyuvali@gmail.com>
2026-09-18 11:14:20 +0300

    Airline Comparison

---
+++ airline-comparison-data.js
@@ -0,0 +1,779 @@
+/* Verified public catalogue and illustrative pricing parameters. See airline-comparison-sources.md. */
+window.airlineComparisonData = {
+  "asOf": "2026-09-17",
+  "isSample": true,
+  "airlines": [
+    {
+      "id": "TK",
+      "name": "Türk Hava Yolları",
+      "short": "THY",
+      "domestic": 1.25,
+      "international": 1.3,
+      "logo": "assets/d18718bb30b2-25px-TK.png"
+    },
+    {
+      "id": "PC",
+      "name": "Pegasus",
+      "short": "Pegasus",
+      "domestic": 1.0,
+      "international": 1.0,
+      "logo": "assets/38122ce71c0f-25px-PC.png"
+    },
+    {
+      "id": "VF",
+      "name": "AJet",
+      "short": "AJet",
+      "domestic": 0.95,
+      "international": 1.04,
+      "logo": "assets/af314a88bf8e-25px-VF.png"
+    },
+    {
+      "id": "XQ",
+      "name": "SunExpress",
+      "short": "SunExpress",
+      "domestic": 0.94,
+      "international": 0.98,
+      "logo": "assets/0f0c494d8b7b-25px-XQ.png"
+    },
+    {
+      "id": "A3",
+      "name": "Aegean Airlines",
+      "short": "Aegean Airlines",
+      "domestic": 0.0,
+      "international": 1.26
+    },
+    {
+      "id": "SU",
+      "name": "Aeroflot",
+      "short": "Aeroflot",
+      "domestic": 0.0,
+      "international": 1.65
+    },
+    {
+      "id": "AM",
+      "name": "Aeromexico",
+      "short": "Aeromexico",
+      "domestic": 0.0,
+      "international": 4.7
+    },
+    {
+      "id": "G9",
+      "name": "Air Arabia",
+      "short": "Air Arabia",
+      "domestic": 0.0,
+      "international": 1.3
+    },
+    {
+      "id": "KC",
+      "name": "Air Astana",
+      "short": "Air Astana",
+      "domestic": 0.0,
+      "international": 1.9
+    },
+    {
+      "id": "AC",
+      "name": "Air Canada",
+      "short": "Air Canada",
+      "domestic": 0.0,
+      "international": 4.1
+    },
+    {
+      "id": "AF",
+      "name": "Air France",
+      "short": "Air France",
+      "domestic": 0.0,
+      "international": 1.75
+    },
+    {
+      "id": "JU",
+      "name": "Air Serbia",
+      "short": "Air Serbia",
+      "domestic": 0.0,
+      "international": 1.2
+    },
+    {
+      "id": "OZ",
+      "name": "Asiana Airlines",
+      "short": "Asiana Airlines",
+      "domestic": 0.0,
+      "international": 4.3
+    },
+    {
+      "id": "J2",
+      "name": "Azerbaijan Airlines",
+      "short": "Azerbaijan Airlines",
+      "domestic": 0.0,
+      "international": 1.28
+    },
+    {
+      "id": "BA",
+      "name": "British Airways",
+      "short": "British Airways",
+      "domestic": 0.0,
+      "international": 1.7
+    },
+    {
+      "id": "CZ",
+      "name": "China Southern Airlines",
+      "short": "China Southern Airlines",
+      "domestic": 0.0,
+      "international": 3.5
+    },
+    {
+      "id": "XC",
+      "name": "Corendon Airlines",
+      "short": "Corendon Airlines",
+      "domestic": 0.0,
+      "international": 0.93
+    },
+    {
+      "id": "EK",
+      "name": "Emirates",
+      "short": "Emirates",
+      "domestic": 0.0,
+      "international": 2.15
+    },
+    {
+      "id": "EY",
+      "name": "Etihad Airways",
+      "short": "Etihad Airways",
+      "domestic": 0.0,
+      "international": 1.85
+    },
+    {
+      "id": "AZ",
+      "name": "ITA Airways",
+      "short": "ITA Airways",
+      "domestic": 0.0,
+      "international": 1.5
+    },
+    {
+      "id": "IB",
+      "name": "Iberia",
+      "short": "Iberia",
+      "domestic": 0.0,
+      "international": 1.65
+    },
+    {
+      "id": "KL",
+      "name": "KLM",
+      "short": "KLM",
+      "domestic": 0.0,
+      "international": 1.6
+    },
+    {
+      "id": "KE",
+      "name": "Korean Air",
+      "short": "Korean Air",
+      "domestic": 0.0,
+      "international": 4.1
+    },
+    {
+      "id": "LO",
+      "name": "LOT Polish Airlines",
+      "short": "LOT Polish Airlines",
+      "domestic": 0.0,
+      "international": 1.4
+    },
+    {
+      "id": "LH",
+      "name": "Lufthansa",
+      "short": "Lufthansa",
+      "domestic": 0.0,
+      "international": 1.7
+    },
+    {
+      "id": "PR",
+      "name": "Philippine Airlines",
+      "short": "Philippine Airlines",
+      "domestic": 0.0,
+      "international": 4.2
+    },
+    {
+      "id": "QR",
+      "name": "Qatar Airways",
+      "short": "Qatar Airways",
+      "domestic": 0.0,
+      "international": 1.95
+    },
+    {
+      "id": "AT",
+      "name": "Royal Air Maroc",
+      "short": "Royal Air Maroc",
+      "domestic": 0.0,
+      "international": 1.7
+    },
+    {
+      "id": "RJ",
+      "name": "Royal Jordanian",
+      "short": "Royal Jordanian",
+      "domestic": 0.0,
+      "international": 1.55
+    },
+    {
+      "id": "SV",
+      "name": "Saudia Airlines",
+      "short": "Saudia Airlines",
+      "domestic": 0.0,
+      "international": 1.55
+    },
+    {
+      "id": "SQ",
+      "name": "Singapore Airlines",
+      "short": "Singapore Airlines",
+      "domestic": 0.0,
+      "international": 4.7
+    },
+    {
+      "id": "PS",
+      "name": "Ukraine Int. Airlines",
+      "short": "Ukraine Int. Airlines",
+      "domestic": 0.0,
+      "international": 0.0
+    },
+    {
+      "id": "UA",
+      "name": "United Airlines",
+      "short": "United Airlines",
+      "domestic": 0.0,
+      "international": 4.3
+    },
+    {
+      "id": "TG",
+      "name": "Thai Airways",
+      "short": "Thai Airways",
+      "domestic": 0.0,
+      "international": 3.7
+    },
+    {
+      "id": "MH",
+      "name": "Malaysia Airlines",
+      "short": "Malaysia Airlines",
+      "domestic": 0.0,
+      "international": 3.8
+    },
+    {
+      "id": "OU",
+      "name": "Croatia Airlines",
+      "short": "Croatia Airlines",
+      "domestic": 0.0,
+      "international": 1.45
+    },
+    {
+      "id": "GQ",
+      "name": "SKY Express",
+      "short": "SKY Express",
+      "domestic": 0.0,
+      "international": 1.2
+    },
+    {
+      "id": "EW",
+      "name": "Eurowings",
+      "short": "Eurowings",
+      "domestic": 0.0,
+      "international": 1.1
+    },
+    {
+      "id": "LX",
+      "name": "Swiss International Air Lines",
+      "short": "Swiss International Air Lines",
+      "domestic": 0.0,
+      "international": 1.8
+    },
+    {
+      "id": "SK",
+      "name": "Scandinavian Airlines",
+      "short": "Scandinavian Airlines",
+      "domestic": 0.0,
+      "international": 1.6
+    },
+    {
+      "id": "AY",
+      "name": "Finnair",
+      "short": "Finnair",
+      "domestic": 0.0,
+      "international": 1.75
+    },
+    {
+      "id": "OS",
+      "name": "Austrian Airlines",
+      "short": "Austrian Airlines",
+      "domestic": 0.0,
+      "international": 1.6
+    },
+    {
+      "id": "EN",
+      "name": "Air Dolomiti",
+      "short": "Air Dolomiti",
+      "domestic": 0.0,
+      "international": 1.7
+    },
+    {
+      "id": "F3",
+      "name": "flyadeal",
+      "short": "flyadeal",
+      "domestic": 0.0,
+      "international": 1.1
+    },
+    {
+      "id": "HY",
+      "name": "Uzbekistan Airways",
+      "short": "Uzbekistan Airways",
+      "domestic": 0.0,
+      "international": 1.85
+    },
+    {
+      "id": "CA",
+      "name": "Air China",
+      "short": "Air China",
+      "domestic": 0.0,
+      "international": 3.5
+    },
+    {
+      "id": "SC",
+      "name": "Shandong Airlines",
+      "short": "Shandong Airlines",
+      "domestic": 0.0,
+      "international": 3.6
+    },
+    {
+      "id": "UX",
+      "name": "Air Europa",
+      "short": "Air Europa",
+      "domestic": 0.0,
+      "international": 1.45
+    },
+    {
+      "id": "TP",
+      "name": "TAP Air Portugal",
+      "short": "TAP Air Portugal",
+      "domestic": 0.0,
+      "international": 1.5
+    },
+    {
+      "id": "VY",
+      "name": "Vueling Airlines",
+      "short": "Vueling Airlines",
+      "domestic": 0.0,
+      "international": 1.12
+    },
+    {
+      "id": "NT",
+      "name": "Binter Canarias",
+      "short": "Binter Canarias",
+      "domestic": 0.0,
+      "international": 1.7
+    },
+    {
+      "id": "DE",
+      "name": "Condor Airlines",
+      "short": "Condor Airlines",
+      "domestic": 0.0,
+      "international": 1.25
+    },
+    {
+      "id": "OR",
+      "name": "TUI fly Netherlands",
+      "short": "TUI fly Netherlands",
+      "domestic": 0.0,
+      "international": 1.2
+    },
+    {
+      "id": "FZ",
+      "name": "flydubai",
+      "short": "flydubai",
+      "domestic": 0.0,
+      "international": 1.6
+    },
+    {
+      "id": "GF",
+      "name": "Gulf Air",
+      "short": "Gulf Air",
+      "domestic": 0.0,
+      "international": 1.8
+    },
+    {
+      "id": "WY",
+      "name": "Oman Air",
+      "short": "Oman Air",
+      "domestic": 0.0,
+      "international": 1.95
+    },
+    {
+      "id": "MS",
+      "name": "Egyptair",
+      "short": "Egyptair",
+      "domestic": 0.0,
+      "international": 1.45
+    },
+    {
+      "id": "ET",
+      "name": "Ethiopian Airlines",
+      "short": "Ethiopian Airlines",
+      "domestic": 0.0,
+      "international": 2.6
+    },
+    {
+      "id": "PW",
+      "name": "Precision Air",
+      "short": "Precision Air",
+      "domestic": 0.0,
+      "international": 3.3
+    },
+    {
+      "id": "TC",
+      "name": "Air Tanzania",
+      "short": "Air Tanzania",
+      "domestic": 0.0,
+      "international": 3.1
+    },
+    {
+      "id": "NH",
+      "name": "All Nippon Airways",
+      "short": "All Nippon Airways",
+      "domestic": 0,
+      "international": 4.4
+    },
+    {
+      "id": "W6",
+      "name": "Wizz Air",
+      "short": "Wizz Air",
+      "domestic": 0,
+      "international": 0.85
+    },
+    {
+      "id": "DP",
+      "name": "Pobeda",
+      "short": "Pobeda",
+      "domestic": 0,
+      "international": 1.05
+    },
+    {
+      "id": "C6",
+      "name": "Centrum Air",
+      "short": "Centrum Air",
+      "domestic": 0,
+      "international": 1.75
+    }
+  ],
+  "cities": [
+    {
+      "id": "0",
+      "name": "Adana",
+      "codes": "COV",
+      "factor": 1.08
+    },
+    {
+      "id": "1",
+      "name": "Adıyaman",
+      "codes": "ADF",
+      "factor": 1.35
+    },
+    {
+      "id": "2",
+      "name": "Ağrı",
+      "codes": "AJI",
+      "factor": 1.48
+    },
+    {
+      "id": "3",
+      "name": "Amasya",
+      "codes": "MZH",
+      "factor": 1.2
+    },
+    {
+      "id": "4",
+      "name": "Ankara",
+      "codes": "ESB",
+      "factor": 1.0
+    },
+    {
+      "id": "5",
+      "name": "Antalya",
+      "codes": "AYT,GZP",
+      "factor": 0.98
+    },
+    {
+      "id": "6",
+      "name": "Artvin",
+      "codes": "RZV",
+      "factor": 1.32
+    },
+    {
+      "id": "7",
+      "name": "Aydın",
+      "codes": "CII",
+      "factor": 0.0
+    },
+    {
+      "id": "8",
+      "name": "Balıkesir",
+      "codes": "EDO,BZI",
+      "factor": 1.18
+    },
+    {
+      "id": "9",
+      "name": "Batman",
+      "codes": "BAL",
+      "factor": 1.38
+    },
+    {
+      "id": "10",
+      "name": "Bingöl",
+      "codes": "BGG",
+      "factor": 1.4
+    },
+    {
+      "id": "11",
+      "name": "Bursa",
+      "codes": "YEI",
+      "factor": 1.24
+    },
+    {
+      "id": "12",
+      "name": "Çanakkale",
+      "codes": "CKZ,GKD",
+      "factor": 1.3
+    },
+    {
+      "id": "13",
+      "name": "Denizli",
+      "codes": "DNZ",
+      "factor": 1.15
+    },
+    {
+      "id": "14",
+      "name": "Diyarbakır",
+      "codes": "DIY",
+      "factor": 1.3
+    },
+    {
+      "id": "15",
+      "name": "Elazığ",
+      "codes": "EZS",
+      "factor": 1.27
+    },
+    {
+      "id": "16",
+      "name": "Erzincan",
+      "codes": "ERC",
+      "factor": 1.25
+    },
+    {
+      "id": "17",
+      "name": "Erzurum",
+      "codes": "ERZ",
+      "factor": 1.3
+    },
+    {
+      "id": "18",
+      "name": "Eskişehir",
+      "codes": "AOE",
+      "factor": 1.27
+    },
+    {
+      "id": "19",
+      "name": "Gaziantep",
+      "codes": "GZT",
+      "factor": 1.12
+    },
+    {
+      "id": "20",
+      "name": "Giresun",
+      "codes": "OGU",
+      "factor": 1.22
+    },
+    {
+      "id": "21",
+      "name": "Hakkari",
+      "codes": "YKO",
+      "factor": 1.55
+    },
+    {
+      "id": "22",
+      "name": "Hatay",
+      "codes": "HTY",
+      "factor": 1.28
+    },
+    {
+      "id": "23",
+      "name": "Iğdır",
+      "codes": "IGD",
+      "factor": 1.53
+    },
+    {
+      "id": "24",
+      "name": "Isparta",
+      "codes": "ISE",
+      "factor": 1.18
+    },
+    {
+      "id": "25",
+      "name": "İstanbul",
+      "codes": "IST,SAW",
+      "factor": 1.0
+    },
+    {
+      "id": "26",
+      "name": "İzmir",
+      "codes": "ADB",
+      "factor": 1.02
+    },
+    {
+      "id": "27",
+      "name": "Kahramanmaraş",
+      "codes": "KCM",
+      "factor": 1.3
+    },
+    {
+      "id": "28",
+      "name": "Kars",
+      "codes": "KSY",
+      "factor": 1.4
+    },
+    {
+      "id": "29",
+      "name": "Kastamonu",
+      "codes": "KFS",
+      "factor": 1.25
+    },
+    {
+      "id": "30",
+      "name": "Kayseri",
+      "codes": "ASR",
+      "factor": 1.09
+    },
+    {
+      "id": "31",
+      "name": "Kocaeli",
+      "codes": "KCO",
+      "factor": 0.0
+    },
+    {
+      "id": "32",
+      "name": "Konya",
+      "codes": "KYA",
+      "factor": 1.13
+    },
+    {
+      "id": "33",
+      "name": "Kütahya",
+      "codes": "KZR",
+      "factor": 1.27
+    },
+    {
+      "id": "34",
+      "name": "Malatya",
+      "codes": "MLX",
+      "factor": 1.29
+    },
+    {
+      "id": "35",
+      "name": "Mardin",
+      "codes": "MQM",
+      "factor": 1.32
+    },
+    {
+      "id": "36",
+      "name": "Mersin",
+      "codes": "COV",
+      "factor": 1.08
+    },
+    {
+      "id": "37",
+      "name": "Muğla",
+      "codes": "BJV,DLM",
+      "factor": 1.16
+    },
+    {
+      "id": "38",
+      "name": "Muş",
+      "codes": "MSR",
+      "factor": 1.42
+    },
+    {
+      "id": "39",
+      "name": "Nevşehir",
+      "codes": "NAV",
+      "factor": 1.25
+    },
+    {
+      "id": "40",
+      "name": "Ordu",
+      "codes": "OGU",
+      "factor": 1.22
+    },
+    {
+      "id": "41",
+      "name": "Rize",
+      "codes": "RZV",
+      "factor": 1.32
+    },
+    {
+      "id": "42",
+      "name": "Samsun",
+      "codes": "SZF",
+      "factor": 1.12
+    },
+    {
+      "id": "43",
+      "name": "Siirt",
+      "codes": "SXZ",
+      "factor": 0.0
+    },
+    {
+      "id": "44",
+      "name": "Sinop",
+      "codes": "NOP",
+      "factor": 1.25
+    },
+    {
+      "id": "45",
+      "name": "Sivas",
+      "codes": "VAS",
+      "factor": 1.24
+    },
+    {
+      "id": "46",
+      "name": "Şanlıurfa",
+      "codes": "GNY",
+      "factor": 1.3
+    },
+    {
+      "id": "47",
+      "name": "Şırnak",
+      "codes": "NKT",
+      "factor": 1.42
+    },
+    {
+      "id": "48",
+      "name": "Tekirdağ",
+      "codes": "TEQ",
+      "factor": 0.0
+    },
+    {
+      "id": "49",
+      "name": "Tokat",
+      "codes": "TJK",
+      "factor": 1.25
+    },
+    {
+      "id": "50",
+      "name": "Uşak",
+      "codes": "USQ",
+      "factor": 0.0
+    },
+    {
+      "id": "51",
+      "name": "Van",
+      "codes": "VAN",
+      "factor": 1.45
+    },
+    {
+      "id": "52",
+      "name": "Zonguldak",
+      "codes": "ONQ",
+      "factor": 1.24
+    }
+  ]
+};
